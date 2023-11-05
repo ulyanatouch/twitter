92fe1ab1307b401c93aaf5cfa7ea37ecea2d2e36
Uliana B. <ulyana9963@gmail.com>
2023-11-05 18:54:04 +0100

    add new changes

--- script.js
+++ script.js
@@ -1,56 +1,55 @@
-// function addPost() {
-//   const title = document.getElementById("postTitle").value;
+document.addEventListener("DOMContentLoaded", (event) => {
+  const heartButton = document.querySelector(".heart-button");
+  const reactionCount = document.querySelector(".reaction-count");
 
-//   fetch("https://dummyjson.com/posts/add", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       title: "I am in love with someone.",
-//       userId: 5,
-//     }),
-//   })
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(data);
-//       alert("Post added successfully!");
-//     })
-//     .catch((err) => {
-//       console.error("Error:", err);
-//       alert("Failed to add post.");
-//     });
-// }
+  heartButton.addEventListener("click", () => {
+    saveReaction();
 
-// Функция для получения данных пользователя по ID
-// async function getUserData(userId) {
-//     const response = await fetch(`https://dummyjson.com/users/${userId}`);
-//     const data = await response.json();
-//     return data;
-//   }
+    const currentCount = parseInt(reactionCount.textContent, 10);
+    reactionCount.textContent = currentCount + 1;
+  });
 
-// Функция для отображения данных на странице
-//   async function displayUserData(userId) {
-//     const userData = await getUserData(userId);
+  function saveReaction() {
+    const reactionData = {
+        postId: postId,
+        userId: userId,
+        reactionType: 'like',
+  }
 
-// Пример того, как можно отобразить полученные данные в HTML
-// const userDisplay = `
-//   <img src="${userData.image}" alt="${userData.firstName}" />
-//   <p>${userData.userName}</p>
-//   <p>${userData.firstName}</p>
-// `;
+  document.addEventListener('DOMContentLoaded', function() {
+    const postButton = document.querySelector('.tweet-button .tweet'); 
+    const postInput = document.getElementById('postTitle'); 
+  
+    postButton.addEventListener('click', function() {
+      const postContent = postInput.value; 
+      const userId = 1;
+    })
+    
+)}
 
-// Добавляем полученные данные на страницу
-//     document.querySelector('.user-container').innerHTML = userDisplay;
-//   }
+  async function createMainUserAddPostForm() {
+    try {
+      const response = await fetch("https://dummyjson.com/users/1");
+      const data = await response.json();
+      const { id, image, username } = data;
 
-// Допустим, вы хотите получить данные пользователя с ID 1
-//   displayUserData(1);
+      const input = document.getElementById("postTitle");
+      const dataObject = {
+        body: input.value,
+        userId: id,
+      };
+     
 
-// Для реакций поста (здесь просто пример, так как не уточнено, какие именно реакции и как их отображать)
-//   const reactions = `
-//     <span>❤️ 57</span>
-//     <span>🔄 24</span>
-//     <span>💬 13</span>
-//   `;
-//   document.querySelector('.reactions-container').innerHTML = reactions;
+      const postResponse = await fetch("https://dummyjson.com/posts/add", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(dataObject),
+      });
+
+      const postResult = await postResponse.json();
+      console.log(postResult);
+    } catch (error) {
+      console.error("Ошибка:", error);
+    }
+  }
+});
