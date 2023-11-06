c22398ebbe4fd561b9014f95b53d411c0f222a3f
Uliana B. <ulyana9963@gmail.com>
2023-11-06 10:07:52 +0100

    new changes

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  // Получаем элементы только один раз, при загрузке документа
+  
   const postButton = document.querySelector(".tweet-button .tweet");
   const postInput = document.getElementById("postTitle");
   const heartButton = document.querySelector(".heart-button");
   const reactionCount = document.querySelector(".reaction-count");
 
-  // Обработчик для кнопки "сердечко"
+
   heartButton.addEventListener("click", () => {
-    const postId = 1; // ID поста должно быть определено или получено откуда-то
-    const userId = 1; // ID пользователя должно быть определено или получено откуда-то
+    const postId = 1; 
+    const userId = 1; 
     saveReaction(postId, userId, "like");
 
     const currentCount = parseInt(reactionCount.textContent, 10);
     reactionCount.textContent = currentCount + 1;
   });
 
-  // Функция для сохранения реакции
+ 
   function saveReaction(postId, userId, reactionType) {
-    // Здесь должна быть логика для сохранения реакции, например, API запрос
+   
     console.log(
-      `Реакция ${reactionType} сохранена для поста ${postId} пользователя ${userId}`
+      `Реакция ${reactionType} для поста ${postId} пользователя ${userId}`
     );
   }
 
-  // Обработчик для кнопки "Tweet"
+
   postButton.addEventListener("click", async () => {
     const postContent = postInput.value;
-    const userId = 1; // ID пользователя должно быть определено или получено откуда-то
+    const userId = 1; 
 
-    try {
       const dataObject = {
         body: postContent,
         userId: userId,
@@ -42,8 +41,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
       const postResult = await postResponse.json();
       console.log(postResult);
-    } catch (error) {
-      console.error("Ошибка:", error);
-    }
+   
   });
 });
+
+
